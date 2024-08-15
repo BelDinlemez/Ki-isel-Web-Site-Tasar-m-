de701a818b3d6c8c9ee79a728630cf7029d0854a
BelDinlemez <131873050+BelDinlemez@users.noreply.github.com>
2024-08-15 14:52:54 +0300

    Add files via upload

---
+++ Kişisel Web Site/func.js
@@ -0,0 +1,73 @@
+function showDiv(value) {
+    const pages = document.querySelectorAll('.page'); 
+    pages.forEach(page => page.classList.add('hidden'));
+    const activePage = document.getElementById(value);
+    if (activePage) {
+        activePage.classList.remove('hidden');
+    }}
+function openModal(a) {
+    const modal = document.getElementById('imageModal');
+    const modalImage = document.getElementById('modalImage');
+    const url = "img/"+a+".jpg"
+    modalImage.src = url;
+    modal.classList.remove('hidden');
+}
+
+function closeModal() {
+    document.getElementById('imageModal').classList.add('hidden');
+};
+//Typing animasyonu
+var Text = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+  };
+  
+  Text.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+  
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+  
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+  
+  
+    if (this.isDeleting) { delta /= 2; }
+  
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+      //console.log("xxxxx")
+    }
+  
+    setTimeout(function() {
+      that.tick();
+    }, delta);
+  };
+  
+  window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-words');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new Text(elements[i], JSON.parse(toRotate), period);
+      }
+    }
+  };
